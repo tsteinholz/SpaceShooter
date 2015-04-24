1c13c66a5c2865417fba93d9ffb0fd4945c333f7
Thomas Steinholz <tsteinholz@yahoo.com>
2015-04-24 16:37:55 -0400

    Travis CI Support

--- core/src/org/southriverhi/space/Levels/SplashScreen.java
+++ core/src/org/southriverhi/space/Levels/SplashScreen.java
@@ -21,7 +21,6 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.FileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
