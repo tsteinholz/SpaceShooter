86fb08f6766908f4a63c0708b7ed82a45940269d
Thomas Steinholz <tsteinholz@yahoo.com>
2015-04-23 20:13:55 -0400

    init commit :)

---
+++ desktop/src/org/southriverhi/space/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.southriverhi.space.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.southriverhi.space.SpaceShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShooter(), config);
+	}
+}
