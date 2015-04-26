ab1300d8144094bf40913ddea0994e3c4c0cac5f
d4rkfly3r <joshua@mrblockplacer.net>
2015-04-26 14:11:43 -0700

    More Server Stuff (Small change)

--- core/src/org/southriverhi/space/Levels/SplashScreen.java
+++ core/src/org/southriverhi/space/Levels/SplashScreen.java
@@ -22,7 +22,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.TimeUtils;
 
