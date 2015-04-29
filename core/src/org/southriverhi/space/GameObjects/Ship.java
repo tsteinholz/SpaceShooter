d6ad0b93bebda15cad236a7d7991cb6d51ef8dec
Thomas Steinholz <tsteinholz@yahoo.com>
2015-04-29 14:24:27 -0400

    Fixed Polymorphism Error.

--- core/src/org/southriverhi/space/GameObjects/Ship.java
+++ core/src/org/southriverhi/space/GameObjects/Ship.java
@@ -1,19 +1,4 @@
-/*
- * This file is part of SpaceShooter.
- *
- *  SpaceShooter is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  SpaceShooter is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with SpaceShooter.  If not, see <http://www.gnu.org/licenses/>.
- */
+
 
 package org.southriverhi.space.GameObjects;
 
@@ -81,5 +66,13 @@ public class Ship extends GameObject {
     @Override
     public float getVelocity() {
         return super.getVelocity();
+    }
+
+    /**
+     * @return the integer Object ID
+     */
+    @Override
+    public int getObjectid() {
+        return 0;
     }
 }
